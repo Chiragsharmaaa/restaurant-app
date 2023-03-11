4de132491eefdec32f7c33b6d77ba9ded90124db
Chirag Sharma <Chiragsharma250999@gmail.com>
2023-03-11 15:06:14 +0530

    Cart open/close through prop drillin

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,7 +3,7 @@ import classes from './Cart.module.css';
 
 const Cart = props => {
     const cartItems = <ul className={classes['cart-items']}>{[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => <li>{item.name}</li>)}</ul>
-    return <Modal>
+    return <Modal onClose={props.onClose}>
         <div>
             {cartItems}
             <div className={classes.total}>
@@ -11,7 +11,7 @@ const Cart = props => {
                 <span>35.62</span>
             </div>
             <div className={classes.actions}>
-                <button className={classes['button--alt']}>Close</button>
+                <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
                 <button className={classes.button}>Order</button>
             </div>
         </div>
